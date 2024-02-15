d3e23346912818382423bd55bd3a6f62248d5f9b
ExequielBaez <exequielbaez@gmail.com>
2024-02-15 15:31:42 -0300

    feat:Swagger/mapperClient

--- back/src/main/java/com/back/chatbot/persistance/mapper/ClientMapper.java
+++ back/src/main/java/com/back/chatbot/persistance/mapper/ClientMapper.java
@@ -7,10 +7,14 @@ import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
 
+import java.util.List;
+
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING,injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 
 public interface ClientMapper {
     ClientEntity toClientEntity(ClientRequestDTO clientRequestDTO);
 
     ClientResponseDTO toClientResponseDto(ClientEntity clientEntity);
+
+    List<ClientResponseDTO> toGetListClientResponseDto(List<ClientEntity> clientEntityList);
 }
