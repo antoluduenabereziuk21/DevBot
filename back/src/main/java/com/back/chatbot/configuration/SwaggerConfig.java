45b4958793014063f7311d02719cb40cdb6ab604
antolube20 <antolube20@gmail.com>
2024-02-27 21:48:32 -0300

    feat: added @OpenAPPiDefinition(servers(@Servers...localhost,donweb) Swagger

--- back/src/main/java/com/back/chatbot/configuration/SwaggerConfig.java
+++ back/src/main/java/com/back/chatbot/configuration/SwaggerConfig.java
@@ -1,6 +1,8 @@
 package com.back.chatbot.configuration;
 
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +10,16 @@ import org.springframework.context.annotation.Configuration;
 
 
 @Configuration
+@OpenAPIDefinition(
+        servers ={ @Server(
+                url = "http://149.50.137.89:9698",//"http://localhost:9698", //http://181.15.143.132:9698
+                description = "dev server don web"),
+                @Server
+                (url = "http://127.0.0.1:9698",//"http://localhost:9698", //http://181.15.143.132:9698
+                description = "development server"),
+
+        }
+)
 public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
@@ -19,6 +31,17 @@ public class SwaggerConfig {
 
                 );
     }
+     /*
+    .contact(new Contact()
+                                .name("Luis Peche")
+                                .url("https://www.linkedin.com/in/luis-peche/"))
+                        .contact(new Contact()
+                                .name("Antonio Bereziuk")
+                                .url("https://www.linkedin.com/in/antonioluduenabereziuk/"))
+                        .contact(new Contact()
+                                .name("Exequiel Baez")
+                                .url("https://www.linkedin.com/in/exequiel-baez-156752238/"))
+     */
 
 }
 
