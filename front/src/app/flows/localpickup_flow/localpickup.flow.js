c180e5988270be0e2c2a25606d6c15594110f731
Rpeche-pk <pecheaparcana1998@gmail.com>
2024-02-29 21:49:27 -0500

    feat(backend) : 🎉 Opcion recojo en tienda

--- front/src/app/flows/localpickup_flow/localpickup.flow.js
+++ front/src/app/flows/localpickup_flow/localpickup.flow.js
@@ -10,12 +10,18 @@ const localpickupFlow = addKeyword(REGEX_KEYWORD, {regex: true})
         idleStart(ctx, gotoFlow, globalState.getMyState().timer);
         await provider.vendor.sendMessage(ctx?.key?.remoteJid, {react: {key: ctx?.key, text: "🤩"}});
     })
+    .addAction(async (ctx, {provider, extensions}) => {
+      //AQUI DEBERIA GENERAR UN PIN DE RECOJO DE LA ORDEN
+    })
     .addAnswer("😉 Sirvase pasar a nuestro local, inmediatamente le enviaremos nuestra *dirección*"
         , {delay: setRandomDelay(800, 550)}
-        , async (ctx, {provider}) => {
+        , async (ctx, {provider, extensions}) => {
             try {
-
-
+                await extensions.utils.typing(provider, {
+                    delay1: setRandomDelay(800, 550),
+                    delay2: setRandomDelay(2500, 1850),
+                    ctx
+                })
                 await provider.vendor.sendMessage(ctx?.key?.remoteJid, {
                     location: {
                         degreesLatitude: -14.070852320268422,
@@ -24,6 +30,7 @@ const localpickupFlow = addKeyword(REGEX_KEYWORD, {regex: true})
                         comment: "Visitanos en familia con tus amigos, te esperamos!"
                     }
                 });
+                await provider.vendor.sendPresenceUpdate("paused", ctx?.key?.remoteJid);
             } catch (error) {
                 console.error(bgRed("ERROR FLUJO onFlow"), error);
                 await postSlack({text: `[ERROR FLUJO ONFLOW:]` + error})
--- front/src/app/flows/order_flow/order.flow.js
+++ front/src/app/flows/order_flow/order.flow.js
@@ -1,13 +1,14 @@
 const {addKeyword, EVENTS} = require('@bot-whatsapp/bot')
 const {setRandomDelay} = require("../../../utils/delay.util");
 const simulateTypingMiddleware = require("../../../middlewares/order.middleware");
-const {idleReset, idleStart} = require("../../../utils/idle.util");
+const {idleReset, idleStart, idleStop} = require("../../../utils/idle.util");
 const {captureDataMiddleware, captureAddressMiddleWare} = require("../../../middlewares/order.middleware");
 const pollMiddleware = require("../../../middlewares/poll.middleware");
 let intents = 2;
 
 const orderFlow = addKeyword(EVENTS.ACTION, {})
     .addAction(async (ctx, {globalState, gotoFlow}) => {
+        idleStop(ctx);
       idleStart(ctx, gotoFlow, globalState.getMyState().timer);
     })
     .addAction(simulateTypingMiddleware)
